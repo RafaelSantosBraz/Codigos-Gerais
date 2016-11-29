3f1314826915da36371adf251862c93377b04670
Rafael dos Santos Braz <rafinha.santos.braz@gmail.com>
2016-11-29 01:35:23 +0000

    Mudanças do exercício de fortune

--- file/fortune/exe.cpp
+++ file/fortune/exe.cpp
@@ -20,17 +20,35 @@ int main(){
     while (arquivo.good()){
         string linha;
         getline(arquivo, linha);
-        if (linha.compare("%") == 0){
+        if (linha[0] != '%'){
             num++;
         }
     }
-    int op_arq = rand() % num + 1;
-    while (arquivo.good()){
-        string linha;
-        getline(arquivo, linha);
-        if (linha.compare("%") == 0){
-            num++;
-        }
+    arquivo.close();
+    if (op_arq == 1){
+        arquivo.open("fortunes", ios::in);
+    } else if(op_arq == 2){
+        arquivo.open("literature", ios::in);
+    } else{
+        arquivo.open("riddles", ios::in);
+    }
+    int frase = rand() % num + 1;
+    cout << frase << endl;
+    cout << op_arq << endl;
+    num = 0;
+    if (frase == 1){
+        
+    } else{
+        while (arquivo.good()){
+            string linha;
+            getline(arquivo, linha);
+            if (num == frase - 1){
+                cout << linha << endl;
+            }
+            if (linha[0] == '%'){
+                num++;
+            }
+        }   
     }
     arquivo.close();
     return 0;
